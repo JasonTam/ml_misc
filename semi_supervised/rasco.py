9d9f209b9d17fb05cb66bb247cb1c9a35283c18f
JasonTam <jasontam22@gmail.com>
2015-08-25 11:52:53 -0400

    finishing up the alg

--- semi_supervised/rasco.py
+++ semi_supervised/rasco.py
@@ -1,11 +1,8 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin, clone
 import itertools
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.linear_model import LinearRegression, LogisticRegression, ElasticNet
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.svm import SVR
 from sklearn.base import clone
+import logging
 
 from time import time
 import sys
@@ -33,7 +30,7 @@ def get_transfer_inds(U_preds, tfer_threshs, tfer_weights):
 
 class Rasco(BaseEstimator, ClassifierMixin):
     """ A Random Subspace Method for Co-Training
-        A semi-supervised model for binary classification
+        A semi-supervised model for classification
     Ref:
         Wang, Jiao, Si-wei Luo, and Xian-hua Zeng.
         "A random subspace method for co-training."
@@ -42,9 +39,9 @@ class Rasco(BaseEstimator, ClassifierMixin):
         IEEE International Joint Conference on. IEEE, 2008.
     """
 
-    def __init__(self, h=None, feat_ratio, n_estimators=8, max_iters=20,
+    def __init__(self, h=None, feat_ratio=0.5, n_estimators=8, max_iters=20,
                  bootstrap=False,
-                 verbose=False):
+                 verbose=False, log_handler=None):
         # todo: immutable arguments please
         """
         :param h: base estimator
@@ -53,9 +50,17 @@ class Rasco(BaseEstimator, ClassifierMixin):
         :param feat_ratio: ratio of features to use per subspace
         :param bootstrap: Iterable of bootstrap resample `n` sample values to use
             per model. If `False`, don't bootstrap
-        :param verbose: verbosity (print timings etc)
+        :param verbose: verbose logging
+        :param log_handler: custom log handler can be passed in
         """
-        self.verbose = verbose
+        self.log = logging.getLogger('rasco')
+        if verbose:
+            self.log.setLevel(logging.DEBUG)
+            self.log.info('RASCO Init')
+            if not self.log.handlers and log_handler:
+                # pass
+                # log_handler = logging.StreamHandler(sys.stdout)
+                self.log.addHandler(log_handler)
 
         self.feat_ratio = feat_ratio
         self.n_estimators = n_estimators
@@ -69,11 +74,28 @@ class Rasco(BaseEstimator, ClassifierMixin):
         self.X_L = None
         self.X_U = None
         self.y_L = None
+        self.classes_ = None
 
         self.estimators = [clone(h) for _ in range(self.n_estimators)]
 
+    def get_transfers(self, preds):
+        """
+        :param preds: shape = (n_ests, n_obs, n_classes)
+        """
+        preds_avg = np.mean(preds, axis=0)
+        y_preds = np.argmax(preds_avg, axis=1)
+        ind = np.argmax(np.max(preds_avg, axis=1), 0)
+        return [ind], y_preds[ind]
+
+    def transfer(self, tfer_inds, y_tfer):
+        X_tfer = self.X_U[tfer_inds, :]
+        self.X_U = np.delete(self.X_U, list(tfer_inds), axis=0)  # Remove pt from set
+        self.X_L, self.y_L = shuffle_unison(
+            np.r_[self.X_L, X_tfer],
+            np.r_[self.y_L, y_tfer],)
+
     def fit_init(self, X, y):
-        n_feats = np.unique(y)
+        n_feats = X.shape[1]
         self.n_feats_subsp = self.feat_ratio * n_feats
         self.sub_sps_inds = [np.random.permutation(n_feats)[:self.n_feats_subsp]
                              for _ in range(self.n_estimators)]
@@ -82,20 +104,21 @@ class Rasco(BaseEstimator, ClassifierMixin):
         self.X_L = X[~ind_u, :]
         self.X_U = X[ind_u, :]
         self.y_L = y[~ind_u]
-
+        self.classes_ = np.unique(self.y_L)
 
     def fit_iter(self):
-        preds = np.zeros()
+        preds = np.zeros((self.n_estimators, len(self.X_U), len(self.classes_)))
         for sub_i in range(self.n_estimators):
             X_L_sub = self.X_L[:, self.sub_sps_inds[sub_i]]
-            y_L_sub = self.y_L[self.sub_sps_inds[sub_i]]
-            self.estimators[sub_i].fit(X_L_sub, y_L_sub)
-            y_pred = self.estimators[sub_i].predict(self.X_U)
-            preds.append(y_pred)
+            self.estimators[sub_i].fit(X_L_sub, self.y_L)
+            probs_pred = self.estimators[sub_i].predict_proba(self.X_U[:, self.sub_sps_inds[sub_i]])
+            preds[sub_i, :, :] = probs_pred
 
+        tfer_inds, y_tfer = self.get_transfers(preds)
 
-        pass
+        self.transfer(tfer_inds, y_tfer)
 
+        return True
 
     def fit(self, X, y):
         """
@@ -107,91 +130,70 @@ class Rasco(BaseEstimator, ClassifierMixin):
         # Begin actual training
         start = time()
         for j in range(self.max_iters):
-            print 'Iter %d' % j
+            self.log.debug('Iter %d' % j)
             tic = time()
 
             ret = self.fit_iter()
 
             if ret:
-                # Replenish U_p
-                # screw it
                 pass
             else:
                 break
 
-            toc = np.round(time() - tic, 2)
-            if self.verbose:
-                print 'Iteration time:', toc
-                sys.stdout.flush()
+            toc = time() - tic
+            self.log.debug('Iter time: %g' % toc)
 
         end = time() - start
-        if self.verbose:
-            print 'Total time:', end
+        self.log.debug('Total time: %g' % end)
+
+    def predict_proba(self, X):
+        return np.mean([est.predict_proba(X[:, sub_sp])
+                        for sub_sp, est in zip(self.sub_sps_inds, self.estimators)],
+                       axis=0)
 
     def predict(self, X):
-        return np.mean([h.predict(X) for h in self.h], axis=0)
+        probs = self.predict_proba(X)
+        return np.argmax(probs, axis=1)
 
+if __name__ == '__main__':
+    from sklearn.neighbors import KNeighborsRegressor
+    from sklearn.linear_model import LinearRegression, LogisticRegression, ElasticNet
+    from sklearn.ensemble import ExtraTreesRegressor
+    from sklearn.svm import SVR
+    from sklearn.metrics import accuracy_score
+    from sklearn.datasets import load_iris
+    iris = load_iris()
+    X, y = iris.data, iris.target
 
+    from sklearn.cross_validation import KFold, StratifiedKFold
 
-sub_sps = [np.random.permutation(n_feats)[:m]
-               for k in range(K)]
+    kf = KFold(n=len(y), n_folds=2)
+    skf = StratifiedKFold(y, n_folds=2)
 
-score_list = []
-score_fn = roc_auc_score
-transfer_counts = []
-for j in range(J):
-    print 'Iter # %d' % j
-#     print 'Winner thresh:', prob_thresh
-    print 'Tfer thresh:', tfer_threshs
-    print 'Tfer weights:', tfer_weights
-    # Subspace creation
-    val_preds = []
+    scores = []
+    scores_base = []
+    for train_ind, test_ind in skf:
+        X_train, X_test = X[train_ind], X[test_ind]
+        y_train, y_test = y[train_ind], y[test_ind]
 
-    ret_iter = Parallel(n_jobs=-1)(
-        delayed(train_predict_model)(
-            L_X[:, sub_sp], L_y,
-            U_X[:, sub_sp],
-            X_valid[:, sub_sp],
-            s_i,
-        )
-        for s_i, sub_sp in enumerate(sub_sps))
+        X_all = np.r_[X_train, X_test]
+        y_all = np.r_[y_train, np.nan*np.ones(len(y_test))]
 
-    models, U_preds, val_preds = zip(*ret_iter)
+        h = LogisticRegression()
+        clf = Rasco(h=h,
+                    feat_ratio=0.5,
+                    n_estimators=8,
+                    max_iters=20,
+                    verbose=True,
+                    log_handler=logging.StreamHandler(sys.stdout))
+        clf.fit(X_all, y_all)
+        y_pred = clf.predict(X_test)
 
-    # Getting the current score for validation set
-    val_pred_ens = np.array(val_preds).mean(axis=0)
-    score_ens = score_fn(y_valid, val_pred_ens[:, 1])
-    score_list.append(score_ens)
-    print 'Ensemble score:', score_ens
+        scores.append(accuracy_score(y_test, y_pred))
 
-    # Current predictions for test set
-#     U_pred_avg = np.array(U_preds).mean(axis=0)  # Ensemble of base preds
-#     max_probs = U_pred_avg.max(axis=1)
+        scores_base.append(accuracy_score(
+            y_test, LogisticRegression().fit(X_train, y_train).predict(X_test)))
 
-#     U_y_pred = np.argmax(U_pred_avg, axis=1)
+    print 'Rasco score:', scores
+    print 'LogisticReg score:', scores_base
 
-#     win_inds = max_probs > prob_thresh
-    win_inds, U_y_pred = get_transfer_inds(U_preds, tfer_threshs, tfer_weights)
-
-    L_X, L_y = shuffle_unison(
-            np.r_[L_X, U_X[win_inds, :]],
-            np.r_[L_y, U_y_pred[win_inds]],)
-
-    U_X = np.delete(U_X, np.where(win_inds), axis=0)
-
-
-    print 'Transferred unlabeled observations: %d' % win_inds.sum()
-    tfer_dist = [list(U_y_pred[win_inds]).count(cc)
-                 for cc in range(n_classes)]
-    print 'Transfer distribution:', [list(U_y_pred[win_inds]).count(cc)
-                                     for cc in range(n_classes)]
-    transfer_counts.append(tfer_dist)
-    print 'Cumulative transfer:', np.array(transfer_counts).sum(axis=0)
-
-    for ii, nt in enumerate(tfer_dist):
-        if nt:
-            tfer_weights[ii] = tfer_weights[ii]*(1+1./nt)/2.
-#     if not win_inds.sum():
-#         prob_thresh -= thresh_decay
-
-    sys.stdout.flush()
