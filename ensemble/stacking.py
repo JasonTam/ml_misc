e78704c05f417c1b49f7b9d997bb20d8c5db317c
Jason Tam <JasonTam22@gmail.com>
2015-08-28 00:22:44 -0400

    more rasco stack compatability

--- ensemble/stacking.py
+++ ensemble/stacking.py
@@ -157,13 +157,13 @@ class Stacking(BaseEstimator, ClassifierMixin):
                     keras_reg_flag = est.config_['layers'][-1]['output_dim'] == 1
                 except (AttributeError, KeyError) as e:
                     pass
-
                 if keras_reg_flag:
                     base_pred = est.predict_proba(X_base_holdout, **base_params['predict'])
                     toc = time.time() - tic
                     self.log.debug('\tTime predict_proba (keras):\t%g s' % toc)
                 elif self.use_probs and hasattr(est, 'predict_proba'):
-                    all_classes = list(np.unique(y_base))
+                    all_classes = list(np.unique(y_base[~np.isnan(y_base)]))
+                    self.log.debug('\Number unique classes:\t%g' % len(all_classes))
                     base_pred_raw = est.predict_proba(X_base_holdout, **base_params['predict'])
                     toc = time.time() - tic
                     base_pred = np.zeros((len(base_pred_raw),
@@ -203,7 +203,11 @@ class Stacking(BaseEstimator, ClassifierMixin):
             X_meta = X_base_preds
         y_meta = y_base_preds
         self.log.debug(':::[Fit meta model %s]:::' % type(self.meta_estimator).__name__)
+        self.log.debug('\tMeta Feat Dimensions:\t\t%s' % str(X_meta.shape))
+        tic = time.time()
         self.meta_estimator.fit(X_meta, y_meta, **meta_params['fit'])
+        toc = time.time() - tic
+        self.log.debug('\tTime fit:\t\t%g s' % toc)
 
         # Retrain the base estimators on entire set
         if self.retrain:
---
+++ examples/rasco_stack.py
@@ -0,0 +1,88 @@
+import sys
+import logging
+import numpy as np
+from sklearn.cross_validation import KFold
+from ensemble.stacking import Stacking
+from semi_supervised.rasco import Rasco
+
+
+if __name__ == '__main__':
+    from sklearn.metrics import mean_squared_error as mse
+    from sklearn.svm import SVR, SVC
+    from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge, Lasso
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.neighbors import KNeighborsRegressor
+    from sklearn.ensemble import ExtraTreesRegressor
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.lda import LDA
+
+    from sklearn.datasets import load_boston
+
+    boston = load_boston()
+    X, y = boston.data, boston.target
+    _, bins = np.histogram(y, bins=10)
+    y_binned = np.digitize(y, bins=bins) - 1
+
+    rasco = Rasco(base_estimator=[DecisionTreeClassifier() for _ in range(8)],
+                  feat_ratio=0.5,
+                  n_estimators=8,
+                  max_iters=5,
+                  n_jobs=1,
+                  verbose=True,
+                  log_handler=logging.StreamHandler(sys.stdout))
+
+    base_ests = [
+        rasco,
+        SVR(),
+        KNeighborsRegressor(n_neighbors=20),
+        # ExtraTreesRegressor(n_estimators=400),
+    ]
+    meta_est = LinearRegression()
+
+    scores_baseline = []
+    scores = []
+    kf = KFold(len(y), n_folds=4)
+    for train_ind, val_ind in kf:
+        X_train_k, X_val_k = X[train_ind], X[val_ind]
+        y_train_k, y_val_k = y[train_ind], y[val_ind]
+        y_binned_train_k, y_binned_val_k = y_binned[train_ind], y_binned[val_ind]
+        X_all_k = np.r_[X_train_k, X_val_k]
+        y_all_k = np.r_[y_binned_train_k, np.nan * np.ones(len(y_binned_val_k))]
+
+        # rasco.y_val = y_binned_val_k
+
+        stack = Stacking(base_ests, meta_est, cv=8,
+                         include_orig_feats=False,
+                         use_probs=True,
+                         fit_params={
+                             'base0_X': 'X_all',
+                             'base0_y': 'y_all',
+                         },
+                         pred_params={
+                         },
+                         extra_data={
+                             'X_all': X_all_k,
+                             'y_all': y_all_k,
+                             'y_binned': y_binned_train_k,
+                         },
+                         verbose=True,
+                         log_handler=logging.StreamHandler(sys.stdout),
+                         save_level0_out=True,
+                         )
+
+        stack.fit(X_train_k, y_train_k)
+
+        y_pred = stack.predict(X_val_k)
+        score = mse(y_val_k, y_pred)
+        print 'MSE:', score
+        scores.append(score)
+
+        # q = ExtraTreesRegressor(n_estimators=400)
+        q = LinearRegression()
+        q.fit(X_train_k, y_train_k)
+        scores_baseline.append(mse(y_val_k, q.predict(X_val_k)))
+
+    print
+    print 'Score mean \t\t\t', np.mean(scores)
+    print 'Score std \t\t\t', np.std(scores)
+    print 'ET baseline \t', np.mean(scores_baseline)
